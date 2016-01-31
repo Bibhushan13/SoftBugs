8946016e7395fca9bab7f61b6fe25653fa6606f9
Bibhushan <bibhushan13@outlook.com>
2016-01-31 12:26:00 +0545

    Tester Description

---
+++ Tester_Test.cs
@@ -0,0 +1,7 @@
+private void CSharpSyntaxHighlight(TextChangedEventArgs e)
+        {
+            txtCode.LeftBracket = '(';
+            txtCode.RightBracket = ')';
+            txtCode.LeftBracket2 = '\x0';
+            txtCode.RightBracket2 = '\x0';
+            //clear style of changed range
